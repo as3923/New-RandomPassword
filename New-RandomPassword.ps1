c3dc7f4532272cddeec4271f1600aca2f11812a3
as3923 <as3923@users.noreply.github.com>
2018-08-10 04:04:18 -0400

    Establishing sets and exclusions

--- New-RandomPassword.ps1
+++ New-RandomPassword.ps1
@@ -13,10 +13,10 @@
     no symbols.
     
 .PARAMETER Length
-    The required length of the password.  
+    The required length of the password.
 
-    Default: 12    
-    Minimum: 3
+    Default: 12
+    Minimum: 4
 
 .EXAMPLE
     New-RandomPassword -Length 8
@@ -27,7 +27,7 @@ function New-RandomPassword {
     [CmdletBinding()]
     Param(
         [Parameter(HelpMessage="Enter minimum length of the password. Minimum 3")]
-        [ValidateScript({$_ -ge 3})]
+        [ValidateScript({$_ -ge 4})]
         [Int] $Length = 16,
         [Parameter(HelpMessage="Exclude symbols (e.g. @#$%)")]
         [Switch] $NoSymbols,
@@ -37,36 +37,59 @@ function New-RandomPassword {
         [Switch] $NoLowercase,
         [Parameter(HelpMessage="Exclude uppercase characters (e.g. ABCDEFGH)")]
         [Switch] $NoUppercase,
-        [Parameter(HelpMessage="Exclude similar characters (e.g. i, l, 1, L, o, 0, O )")]
+        [Parameter(HelpMessage="Exclude similar characters (e.g. i, l, 1, L, o, 0, O)")]
         [Switch] $NoSimilarChars,
-        [Parameter(HelpMessage="Exclude ambiguous characters ({ } [ ] ( ) / \ ' "" ` ~ , ; : . < > )")]
+        [Parameter(HelpMessage="Exclude ambiguous characters ({ } [ ] ( ) / \ ' "" ` ~ , ; : . < >)")]
         [Switch] $NoAmbiguousChars
     )
     BEGIN {
+        $set = @()
+        $exclusions = @()
         ### Symbols ###
-        33..47
-        58..64
-        91..96
-        123..126
-        161..255
+        if ($NoSymbols) {
+        } else {
+            $set += 33..47
+            $set += 58..64
+            $set += 91..96
+            $set += 123..126
+            #$set += 161..255
+        }
 
         ### Numbers ###
-        48..57
+        if ($NoNumbers) {
+        } else {
+            $set += 48..57
+        }
 
         ### Lowercase ###
-        97..122
-        
+        if ($NoLowercase) {
+        } else {
+            $set += 97..122
+        }
+
         ### Uppercase ###
-        65..90
+        if ($NoUppercase) {
+        } else {
+            $set += 65..90
+        }
 
         ### Similar Characters ###
-        
-        ### Ambiguous Characters ###        
+        if ($NoSimilarChars) {
+            $exclusions += 48,49,73,79,105,108,111,124
+        }
+
+        ### Ambiguous Characters ###
+        if ($NoAmbiguousChars) {
+            $exclusions += 34,39,40,41,44,46,47,58,59,60,62,91,92,93,96,123,125,126
+        }
+
+        if ($set.Count -eq 0) {
+            Throw "There are no characters to create a password with. Try using less exclusions."
+        }
 
     }
     PROCESS {
-#        Add-Type -AssemblyName System.Web
-#        $randomPassword = [System.Web.Security.Membership]::GeneratePassword($Length,1)$numbers=$null
+
         $randomPassword
     }
 }
